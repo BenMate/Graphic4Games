901d9887322fe78ee8d40395fb0b421fd9076d16
BenMate <benscott98@hotmail.com>
2022-04-20 17:32:37 +1000

    test1

---
+++ UnityTutorials/HLSL_Shader/Assets/scripts/PostProcessor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessor : MonoBehaviour
+{
+    public Material mat;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
